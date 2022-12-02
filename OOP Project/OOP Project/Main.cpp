5bf69c30d5add7ea2031b73cd6c0270a73dfddd6
Patainea Florinel-Robert <patainearobert@yahoo.com>
2022-12-02 10:43:18 +0200

    validate ticket

--- OOP Project/OOP Project/Main.cpp
+++ OOP Project/OOP Project/Main.cpp
@@ -3,7 +3,7 @@
 #include "Event.h"
 #include "Menu.h"
 #include "TicketIssuer.h"
-
+#include "TicketValidator.h"
 
 using namespace std;
 
@@ -11,6 +11,7 @@ int main() {
 	EventStore store;
 	TicketIssuer issuer;
 	Menu menu;
+	TicketValidator validator;
 
 	store.initialize();
 	TicketArray ticketArray = menu.getTickets(store, issuer);
@@ -18,6 +19,17 @@ int main() {
 
 	for (int i = 0; i < ticketArray.getSize(); i++) {
 		cout << i + 1 << ". " << tickets[i].getId() << endl;
+	}
+
+	Ticket randomTicket = Ticket("123", &store.getEvent(0));
+
+	bool isTicketValid = validator.isTicketValid(randomTicket);
+
+	if (isTicketValid) {
+		cout << "Your ticket is valid!";
+	}
+	else {
+		cout << "Your ticket is invalid!";
 	}
 
 	return 0;
